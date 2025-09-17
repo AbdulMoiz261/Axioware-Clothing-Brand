36710c55c83ebd9e809513fda11c86cf9cbdc258
Abdul Moiz <abdulmoiznoman08@gmail.com>
2025-09-18 04:32:19 +0500

    fix issues

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import HeroSection from './components/HeroSection';
 import CategorySection from './components/CategorySection';
 import WomenPage from './components/WomenPage';
 import MenPage from './components/MenPage';
-import KidsPage from './components/KidsPage';
 import MenProductDetail from './components/MenProductDetail';
 import WomenProductDetail from './components/WomenProductDetail';
-import KidsProductDetail from './components/KidsProductDetail';
 
 function AppContent() {
   const [currentPage, setCurrentPage] = useState('home');
@@ -31,7 +29,11 @@ function AppContent() {
 
   const MenPageComponent = () => <MenPage />;
 
-  const KidsPageComponent = () => <KidsPage />;
+  const KidsPage = () => (
+    <div>
+      <CategorySection category="Kids" />
+    </div>
+  );
 
   const SalePage = () => (
     <div>
@@ -46,11 +48,10 @@ function AppContent() {
         <Route path="/" element={<HomePage />} />
         <Route path="/women" element={<WomenPage />} />
         <Route path="/men" element={<MenPageComponent />} />
-        <Route path="/kids" element={<KidsPageComponent />} />
+        <Route path="/kids" element={<KidsPage />} />
         <Route path="/sale" element={<SalePage />} />
         <Route path="/product/men/:id" element={<MenProductDetail />} />
         <Route path="/product/women/:id" element={<WomenProductDetail />} />
-        <Route path="/product/kids/:id" element={<KidsProductDetail />} />
       </Routes>
     </div>
   );
